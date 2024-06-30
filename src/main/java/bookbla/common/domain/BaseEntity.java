48fc5e7700e01457d25e5159d0aea99db9e85d9e
iamjooon2 <iamjooon2@gmail.com>
2024-07-01 05:06:14 +0900

    feat: soft-delete 적용

--- src/main/java/bookbla/common/domain/BaseEntity.java
+++ src/main/java/bookbla/common/domain/BaseEntity.java
@@ -7,8 +7,6 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SoftDelete;
-import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,8 +14,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-@SoftDelete(columnName = "isDeleted")
-@Where(clause = "deleted = false")
 @MappedSuperclass
 @Getter
 public abstract class BaseEntity {
@@ -30,9 +26,10 @@ public abstract class BaseEntity {
 
     private boolean isDeleted = false;
 
-    protected void delete() {
+    private LocalDateTime deletedAt;
+
+    public void delete(LocalDateTime now) {
         this.isDeleted = true;
+        this.deletedAt = now;
     }
-
 }
-
